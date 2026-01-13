471ea9fbc12b765f25819668a897f12db779995d
keshiacor <keshiacorioll@me.com>
2026-01-12 20:30:48 -0500

    Adds validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 8 }, on: :create
+  validates :email, presence: true, uniqueness: true
+  has_many :posts
 end
