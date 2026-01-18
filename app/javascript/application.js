df9c2b6ffee55c696af8e101f7977bc693453855
keshiacor <keshiacorioll@me.com>
2026-01-18 16:14:48 -0500

    adds styling to the entire app

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,22 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import "@hotwired/turbo-rails";
+import "controllers";
+
+document.addEventListener("turbo:load", () => {
+  const flashes = document.querySelectorAll(".notice, .alert");
+
+  flashes.forEach((flash) => {
+    setTimeout(() => {
+      flash.classList.add("flash-hide");
+
+      // Remove from DOM after fade-out finishes
+      flash.addEventListener(
+        "transitionend",
+        () => {
+          flash.remove();
+        },
+        { once: true }
+      );
+    }, 3000); // 3 seconds before starting fade-out
+  });
+});
